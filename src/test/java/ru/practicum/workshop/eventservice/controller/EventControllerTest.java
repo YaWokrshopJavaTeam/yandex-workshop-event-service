4d9f6b5337a901d8e25d94b2ce297cf336bf4c4a
Druzhinin Yury <druzhinin.yy@gmail.com>
2024-11-27 04:04:38 +0300

    tests: added integration tests for org team controller and service.

--- src/test/java/ru/practicum/workshop/eventservice/controller/EventControllerTest.java
+++ src/test/java/ru/practicum/workshop/eventservice/controller/EventControllerTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(controllers = EventController.class)
 public class EventControllerTest {
 
     @Autowired
